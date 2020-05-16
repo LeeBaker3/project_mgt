d8152ef13747531347f01a1eb89f61989dbec021
Lee Baker <lee.baker1@ymail.com>
2020-05-16 18:33:59 +1000

    Base project structure. Added custom user model and tests

---
+++ users/tests.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+# Create your tests here.
+
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='test_user',
+            email='test_email@gmail.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(user.username, 'test_user')
+        self.assertEqual(user.email, 'test_email@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@gmail.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(user.username, 'superadmin')
+        self.assertEqual(user.email, 'superadmin@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
